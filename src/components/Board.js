1302fadb3e147623b995b665b1e3ea5dce1a8206
Niklas <zhoie@icloud.com>
2023-03-03 23:50:55 +0800

    show X after click button

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,7 +3,7 @@ import Square from "./Square";
 
 class Board extends React.Component{
     renderSquare(i) {
-        return <Square value = {i} />;
+        return <Square />;
     }
 
     render() {
