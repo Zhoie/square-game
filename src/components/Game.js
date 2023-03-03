12d81fe40edee5e9de300a05d2fdba83f16738d0
Niklas <zhoie@icloud.com>
2023-03-03 23:51:33 +0800

    x

--- src/components/Game.js
+++ src/components/Game.js
@@ -4,9 +4,13 @@ import Board from "./Board";
 class Game extends React.Component {
     render(){
         return (
-            <div class="game">
-                <div class="game-board">
+            <div className="game">
+                <div className="game-board">
                     <Board />
+                </div>
+                <div className="game-info">
+                    <div>{/*status*/}</div>
+                    <ol>{/*TODO */}</ol>
                 </div>
             </div>
         )
--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import ShoppingList from './components/ShoppingList';
-import Square from './components/Square';
 import Game from './components/Game';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -14,7 +10,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
